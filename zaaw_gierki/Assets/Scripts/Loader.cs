3d1c3fef31ff1ddd2eda0d4f6152e123c4adee95
Elterian <209963@student.pwr.edu.pl>
2017-11-26 22:37:23 +0100

    created prefabs and started scripts

---
+++ zaaw_gierki/Assets/Scripts/Loader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class Loader : MonoBehaviour
+    {
+        public GameObject gameManager;          //GameManager prefab to instantiate.
+        public GameObject soundManager;         //SoundManager prefab to instantiate.
+
+
+        void Awake()
+        {
+            //Check if a GameManager has already been assigned to static variable GameManager.instance or if it's still null
+            if (GameManager.instance == null)
+
+                //Instantiate gameManager prefab
+                Instantiate(gameManager);   
+        }
+    }
+}
